cab741f6ce0348e34b6930146daadc2fd52a581b
DiguyaDeveloper <diegocecconcwb@outlook.com>
2020-07-24 20:25:02 -0300

    Adicionando V1 projeto

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,95 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { Nav } from "reactstrap";
+// javascript plugin used to create scrollbars on windows
+import PerfectScrollbar from "perfect-scrollbar";
+
+import logo from "logo.svg";
+
+var ps;
+
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.activeRoute.bind(this);
+    this.sidebar = React.createRef();
+  }
+  // verifies if routeName is the one active (in browser input)
+  activeRoute(routeName) {
+    return this.props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+  }
+  componentDidMount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps = new PerfectScrollbar(this.sidebar.current, {
+        suppressScrollX: true,
+        suppressScrollY: false,
+      });
+    }
+  }
+  componentWillUnmount() {
+    if (navigator.platform.indexOf("Win") > -1) {
+      ps.destroy();
+    }
+  }
+  render() {
+    return (
+      <div
+        className="sidebar"
+        data-color={this.props.bgColor}
+        data-active-color={this.props.activeColor}
+      >
+        <div className="sidebar-profile">
+          <div className="sidebar-profile-info">
+            <div className="logo-img">
+              <img src={logo} alt="react-logo" />
+            </div>
+            <p> João Marcelo </p>
+            <p> Curitiba - PR </p>
+          </div>
+        </div>
+        <div className="logo">
+          <a
+            href="https://www.creative-tim.com"
+            className="simple-text logo-mini"
+          >
+            <div className="logo-img">
+              <img src={logo} alt="react-logo" />
+            </div>
+          </a>
+          <a
+            href="https://www.creative-tim.com"
+            className="simple-text logo-normal"
+          >
+            TEAM MICRO
+          </a>
+        </div>
+        <div className="sidebar-wrapper" ref={this.sidebar}>
+          <Nav>
+            {this.props.routes.map((prop, key) => {
+              return (
+                <li
+                  className={
+                    this.activeRoute(prop.path) +
+                    (prop.pro ? " active-pro" : "")
+                  }
+                  key={key}
+                >
+                  <NavLink
+                    to={prop.layout + prop.path}
+                    className="nav-link"
+                    activeClassName="active"
+                  >
+                    <i className={prop.icon} />
+                    <p>{prop.name}</p>
+                  </NavLink>
+                </li>
+              );
+            })}
+          </Nav>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Sidebar;
---
+++ src/routes.js
@@ -0,0 +1,45 @@
+import Dashboard from "views/Dashboard.js";
+import Notifications from "views/Notifications.js";
+import UserPage from "views/User.js";
+import UpgradeToPro from "views/Upgrade.js";
+import RegisterUser from "views/RegisterUser";
+
+var routes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "nc-icon nc-bank",
+    component: Dashboard,
+    layout: "/admin",
+  },
+  {
+    path: "/icons",
+    name: "My Profile",
+    icon: "fa fa-user",
+    component: RegisterUser,
+    layout: "/admin",
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: "nc-icon nc-bell-55",
+    component: Notifications,
+    layout: "/admin",
+  },
+  {
+    path: "/user-page",
+    name: "User Profile",
+    icon: "nc-icon nc-single-02",
+    component: UserPage,
+    layout: "/admin",
+  },
+  {
+    pro: true,
+    path: "/upgrade",
+    name: "LOGOUT",
+    icon: "nc-icon nc-spaceship",
+    component: Dashboard,
+    layout: "/admin",
+  },
+];
+export default routes;
