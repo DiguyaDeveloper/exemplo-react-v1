b9d590f38bb152e261394b12c219c7f38ae5f9a9
Lucas Gabriel <lucas.gabriel@sinqia.com.br>
2020-07-31 03:11:27 -0300

    funções dinamicas na aplicação
    Signed-off-by: Lucas Gabriel <lucas.gabriel@sinqia.com.br>

--- src/views/Hashtag.js
+++ src/views/Hashtag.js
@@ -1,4 +1,9 @@
-import React from "react";
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { HashtagService } from "../service/Hashtag.service";
+
+
+
 
 // reactstrap components
 import {
@@ -11,44 +16,79 @@ import {
     Col,
 } from "reactstrap";
 
+/*const [hashtags, setHashtags] = useState([]);*/
+
+
 
 class Dashboard extends React.Component {
+    response;
+
+
+    constructor() {
+        super();
+        this.hashtagService = new HashtagService();
+        this.state = {
+            hashtags: [],
+        };
+
+    }
+
+
+    async getUser() {
+        this.hashtagService.getHashtag().then((res) => {
+            console.log(res);
+
+
+            this.setState({ hashtags: res.data });
+            console.log('stado hastag', this.state.hashtags);
+
+        });
+    };
+
     render() {
+        const { hashtags } = this.state;
+        this.getUser();
+
         return (
             <>
                 <div className="content">
                     <div class="body-content">
-                        <div class="card-item">
-                            <a href="">
-                                <Card className="card-stats">
-                                    <CardBody>
-                                        <Row>
-                                            <Col md="2" xs="3">
-                                                <div className="icon-big text-center icon-warning">
-                                                    <div className="content-icon-hastag">
-                                                        <i class="fa fa-hashtag" aria-hidden="true"></i>
+
+                        {this.state.hashtags && this.state.hashtags.map(hashtag => (
+
+
+                            <div class="card-item" id={hashtag._id}>
+                                <a href="">
+                                    <Card className="card-stats">
+                                        <CardBody>
+                                            <Row>
+                                                <Col md="2" xs="3">
+                                                    <div className="icon-big text-center icon-warning">
+                                                        <div className="content-icon-hastag">
+                                                            <i class="fa fa-hashtag" aria-hidden="true"></i>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </Col>
-                                            <Col md="10" xs="7">
-                                                <div className="info">
-                                                    <p className="title">Ansiedade</p>
-                                                    <div>
-                                                        <p className="descript">A ansiedade pode ser normal e é um indicador de doença subjacente somente quando os sentimentos se tornam excessivos, obsessivos e interferirem na vida cotidiana.</p>
+                                                </Col>
+                                                <Col md="10" xs="7">
+                                                    <div className="info">
+                                                        <p className="title">{hashtag.description}</p>
+                                                        <div>
+                                                            <p className="descript">A ansiedade pode ser normal e é um indicador de doença subjacente somente quando os sentimentos se tornam excessivos, obsessivos e interferirem na vida cotidiana.</p>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </Col>
-                                        </Row>
-                                    </CardBody>
-                                    <CardFooter>
-                                        <hr />
-                                        <div className="stats">
-                                            <span>10</span> grupos
-                                        </div>
-                                    </CardFooter>
-                                </Card>
-                            </a>
-                        </div>
+                                                </Col>
+                                            </Row>
+                                        </CardBody>
+                                        <CardFooter>
+                                            <hr />
+                                            <div className="stats">
+                                                <span>10</span> grupos
+                                            </div>
+                                        </CardFooter>
+                                    </Card>
+                                </a>
+                            </div>
+                        ))}
                         <div class="card-item">
                             <a href="">
                                 <Card className="card-stats">
